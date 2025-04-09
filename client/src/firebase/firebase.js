32baa5e5491df19d6a766c89862cc6e78638eadc
Nirvanjha2004 <nirvanjha2004@gmail.com>
2025-04-09 11:23:05 +0530

    added google analytics

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDqb3m2wwnY2yNhZWzR6UDTnP-AI4I7gLg",
+    authDomain: "vashisht-84542.firebaseapp.com",
+    projectId: "vashisht-84542",
+    storageBucket: "vashisht-84542.firebasestorage.app",
+    messagingSenderId: "723317032655",
+    appId: "1:723317032655:web:ef33ea3ce5fd5138f319ba",
+    measurementId: "G-BDHJ1PW01Y"
 };
 
 // Add console logging to help debug Firebase initialization
